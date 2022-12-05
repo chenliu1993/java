64860879f3922b340cb327c1a7ac1b110e3294e3
chenliu1993 <lc17319031387@163.com>
2022-12-05 21:15:34 +0800

    Fix package definition issue
    For example the package name is `package com.learning.day1`,
    then user should right-click the src/main/java and select `add folder to java source path`
    if there are any other child folders are already added to source path, remove it first
    
    This is really not the same as golang and this is not convenient.....
    
    Signed-off-by: chenliu1993 <lc17319031387@163.com>

--- src/main/java/com/learning/day1/BubbleSort.java
+++ src/main/java/com/learning/day1/BubbleSort.java
@@ -3,7 +3,8 @@ package com.learning.day1;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,1};
+        int[] arr = new int[]{4,5,6,7,1};
+
         for(int i=1, size=arr.length;i<size;i++) {
             boolean swap = false;
             for(int j=0;j<size-i;j++) {
@@ -21,7 +22,9 @@ public class BubbleSort {
                 break;
             }
         }
-        System.out.println(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
     
 }
--- src/main/java/com/learning/day1/HelloWorld.java
+++ src/main/java/com/learning/day1/HelloWorld.java
@@ -1,5 +1,5 @@
-package com.learning.day1;
 
+package com.learning.day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
